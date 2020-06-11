30444012d194cf6aab51a2329b66040ebe7b8748
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2020-06-11 10:23:31 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
+import { AmplifyGreetings, withAuthenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AmplifyGreetings username={Auth?.user.username}></AmplifyGreetings>
+      App
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import Amplify from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import aws_exports from './aws-exports';
 import * as serviceWorker from './serviceWorker';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
